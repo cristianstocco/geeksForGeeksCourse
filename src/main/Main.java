15fd73c4394fab81072d8f759f65f22eb1e93d8d
cristianstocco <stykky@stykky.fritz.box>
2022-09-08 16:04:36 +0200

    added all the computations for the exercizes

--- src/main/Main.java
+++ src/main/Main.java
@@ -4,11 +4,18 @@
 package main;
 
 import arraylist.ArrayListMain;
+import arrays.ArraysMain;
+import collections.CollectionsMain;
+import comparator.ComparatorMain;
 import lambda.LambdaMain;
 import linkedlist.LinkedListMain;
+import map.MapMain;
 import queue.QueueMain;
+import set.SetMain;
+import sort.SortMain;
 import stack.StackMain;
 import stream.StreamMain;
+import string.StringMain;
 
 /**
  * @author stykky
@@ -33,6 +40,13 @@ public class Main {
 		new LinkedListMain().execute();
 		new StackMain().execute();
 		new QueueMain().execute();
+		new SetMain().execute();
+		new MapMain().execute();
+		new StringMain().execute();
+		new ComparatorMain().execute();
+		new ArraysMain().execute();
+		new CollectionsMain().execute();
+		new SortMain().execute();
 	}
 
 }
